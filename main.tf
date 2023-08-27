1a80fc8f57cd57f907192b545fd0a03dea54acb4
idoshoy <105215042+idoshoy@users.noreply.github.com>
2023-08-27 16:40:42 +0300

    דשגכדג

--- main.tf
+++ main.tf
@@ -23,33 +23,31 @@ module "blog_vpc" {
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.10.0"
-  # insert the 1 required variable here
-  
-  name = "blog"
-  min_size = 1
-  max_size = 2
 
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-  
-  image_id            = data.aws_ami.app_ami.id
   instance_type       = var.instance_type
-  }
+  image_id            = data.aws_ami.app_ami.id
+}
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
+  version = "~> 6.0"
 
   name = "blog-alb"
 
@@ -65,7 +63,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      
     }
   ]
 
@@ -84,7 +81,7 @@ module "blog_alb" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
+  version = "4.13.0"
 
   vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
